c11f919440a925c5dcb1edc2e16459269a2b91d3
sys <sys@qq.com>
2019-11-22 22:49:41 +0800

    第一提交

---
+++ src/cn/ndqm/Action/LoginAction.java
@@ -0,0 +1,13 @@
+package cn.ndqm.Action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction  extends  ActionSupport {
+   
+	public  String login() {
+		
+		return SUCCESS;
+	}
+	
+	
+}
